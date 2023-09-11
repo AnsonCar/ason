ef7aba21b190a59607cf5c622e9426f703fc7c90
AnsonCar <anson1tsang@gmail.com>
2023-09-11 15:51:46 +0800

    ason 2023-09-11 (v1.1.6)

--- dirTree.py
+++ dirTree.py
@@ -1,45 +1,34 @@
 import os
 import sys
 
-def dirTree(path, exclude_list=None, max_depth=None, current_depth=0, is_last=False):
-    # Get the list of files and directories in the given path
+def dirTree(path, exclude_list=None, max_depth=None, current_depth=0, isNext=False):
+    # 获取给定路径下的文件和目录列表
     files = os.listdir(path)
-    # Loop through the list
+    # 遍历列表
     for i, file in enumerate(files):
-        # Exclude files or directories in exclude_list
+        # 排除在exclude_list中的文件或目录
         if exclude_list is not None and file in exclude_list:
             continue
-        # If the file is a directory and we haven't exceeded the max depth
-        if os.path.isdir(os.path.join(path, file)) and (max_depth is None or current_depth < max_depth):
-            # Print the directory name with the appropriate prefix
-            prefix = "└── " if (i == len(files) - 1 and is_last) else "├── "
-            print("    " * current_depth + prefix + file)
-            # Recursively call the function with the subdirectory as the path and increased depth
-            is_last_subdir = i == len(files) - 1
-            dirTree(os.path.join(path, file), exclude_list, max_depth, current_depth + 1, is_last_subdir)
-        # If the file is a file or we've exceeded the max depth
+        # # 判断是否为最后一项
+        if isNext:
+            is_last = len(files) - 1 == i
         else:
-            # Print the file name with the appropriate prefix
-            prefix = "└── " if (i == len(files) - 1 and is_last) else "├── "
-            print("    " * current_depth + prefix + file)
+            is_last = len(files) - 2 == i
+        # # 打印前缀
+        prefix = "└── " if is_last else "├── "
+        # # 打印文件或目录名
+        print("    " * current_depth + prefix + file)
+        # # 如果是目录且未超过最大深度
+        if os.path.isdir(os.path.join(path, file)) and (max_depth is None or current_depth < max_depth):
+            # 递归调用函数，将子目录作为路径，深度增加
+            dirTree(os.path.join(path, file), exclude_list, max_depth, current_depth + 1, isNext=True)
 
-# Get the command-line arguments
+# 获取命令行参数
 argv = sys.argv
 exclude_list = [".DS_Store", ".git", "node_modules"]
 
+print("*")
 if len(argv) > 2:
-    dirTree(argv[1], exclude_list=exclude_list, max_depth=int(argv[2]) - 1, is_last=True)
+    dirTree(argv[1], exclude_list=exclude_list, max_depth=int(argv[2]) - 1)
 else:
-    dirTree(argv[1], exclude_list=exclude_list, is_last=True)
-    
-# │
-# ├── LICENSE 開源證明
-# ├── README.md 項目介紹
-# ├── docs 文檔
-# │   ├── Logo
-# │   └── changelog.md 開發日誌
-# │
-# ├── linux_init
-# ├── nodejs
-# ├── py_init
-# └── vuePress_init
+    dirTree(argv[1], exclude_list=exclude_list)
